ce1c04b330e0aa9cfcbe588054b9a6dfc01e72a2
Lucas Julian Güerci <ljguerci@teco.com.ar>
2020-02-15 14:23:44 -0300

    Code cleanup

--- src/App.jsx
+++ src/App.jsx
@@ -1,20 +1,26 @@
 import React from 'react';
 import './App.css';
-import Login from './components/login'
+
+/* Import Store and reducers */
 import { Provider } from 'react-redux';
 import { createStore, applyMiddleware  } from 'redux';
-import loginApp from './_reducers';
 import thunkMiddleware from 'redux-thunk';
-import { Home } from './components/home';
+import loginApp from './_reducers';
+
+/* Import Router and helpers */
 import { Router, Route, Switch, Redirect } from 'react-router-dom';
 import { history } from './_helpers';
-import { PrivateRoute } from './components';
+import { PrivateRoute } from './_components';
 
+/* Import APP components */
+import { Home } from './components/home';
+import Login from './components/login';
 
 function App() {
 
+  /* Create Store and apply thunk to enable ASYNC actions */
   const store = createStore(loginApp,applyMiddleware(
-    thunkMiddleware // lets us dispatch() functions
+    thunkMiddleware
   ));
 
 
@@ -25,7 +31,6 @@ function App() {
             <Switch>
                 <PrivateRoute exact path="/" component={Home} />
                 <Route path="/login" component={Login} />
-          {/*       <Route path="/register" component={RegisterPage} /> */}
                 <Redirect from="*" to="/" />
             </Switch>
         </Router>
